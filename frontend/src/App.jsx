e7e332fc13a8c138b9c6ade4848df19612ea8bfc
kevin181719 <vaghasiyakevin49@gmail.com>
2025-11-13 11:40:21 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home/Home";
-import Cart from './pages/Cart/cart';
+import Cart from './pages/Cart/Cart';
 import Placeorder from './pages/Placeorder/Placeorder';
 import Footer from './components/Footer/Footer';
 import Loginpopup from './components/Login/Loginpopup';
